a04c6a789972c5b8edab7f9c468573715b4f5b9b
LohasOT <oliverhoang93@gmail.com>
2021-12-04 15:01:43 -0800

    update generate markd

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,15 +3,52 @@ function generateMarkdown(data) {
   return `
   # ${data.title}
 
-  ## Description
-  ${data.description}
-
   ##Table of Content
 
   * [Installation](#installation)
   * [Usage](#usage)
   * [Contributing](#contributing)
   * [Tests](#tests)
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+
+  *Steps required to install project and how to get the development environment running:*
+
+  ${data.installation}
+
+  ## Usage
+
+  *Instructions and examples for use:*
+
+  ${data.usage}
+
+  ## Contributing
+
+  *If you would like to contribute it, you can follow these guidelines for how to do so.*
+
+  ${data.contributing}
+
+  ## Tests
+
+  *Tests for application and how to run them:*
+
+  ${data.tests}
+
+  ## License
+
+  ${data.license}
+
+  ## Questions?
+
+
+  For any questions, please contact me with the information below:
+
+  GitHub: ${data.username}
+
+  Email: ${data.email}
 `;
 }
 
