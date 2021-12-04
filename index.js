a6c766680586fed5922b8564c31bdc62479f5579
LohasOT <oliverhoang93@gmail.com>
2021-12-04 13:46:49 -0800

    restart

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-// External packages
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
-
-// Internal modules
-const api = require('./utils/api.js');
 const generateMarkdown = require('./utils/generateMarkdown.js');
-
-// Inquirer prompts for userResponses
+// array of questions for user
 const questions = [
   {
     type: 'input',
     message: "What is your GitHub username? (No @ needed)",
     name: 'username',
-    default: 'oliverhoang',
     validate: function (answer) {
       if (answer.length < 1) {
         return console.log("A valid GitHub username is required.");
@@ -25,7 +19,6 @@ const questions = [
     type: 'input',
     message: "What is the name of your GitHub repo?",
     name: 'repo',
-    default: 'readme-generator',
     validate: function (answer) {
       if (answer.length < 1) {
         return console.log("A valid GitHub repo is required for a badge.");
@@ -37,7 +30,6 @@ const questions = [
     type: 'input',
     message: "What is the title of your project?",
     name: 'title',
-    default: 'Project Title',
     validate: function (answer) {
       if (answer.length < 1) {
         return console.log("A valid project title is required.");
@@ -49,7 +41,6 @@ const questions = [
     type: 'input',
     message: "Write a description of your project.",
     name: 'description',
-    default: 'Project Description',
     validate: function (answer) {
       if (answer.length < 1) {
         return console.log("A valid project description is required.");
@@ -80,48 +71,24 @@ const questions = [
   {
     type: 'list',
     message: "Choose a license for your project.",
-    choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense','None'],
+    choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense', 'None'],
     name: 'license'
+  },
+  {
+    type: 'input',
+    message: "Provide Your Email"
+    name: 'Email'
   }
 ];
 
+// function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, err => {
-    if (err) {
-      return console.log(err);
-    }
-
-    console.log("Success! Your README.md file has been generated")
-  });
 }
 
-const writeFileAsync = util.promisify(writeToFile);
+// function to initialize program
+function init() {
 
+}
 
-// Main function
-async function init() {
-  try {
-
-    // Prompt Inquirer questions
-    const userResponses = await inquirer.prompt(questions);
-    console.log("Your responses: ", userResponses);
-    console.log("Thank you for your responses! Fetching your GitHub data next...");
-
-    // Call GitHub api for user info
-    const userInfo = await api.getUser(userResponses);
-    console.log("Your GitHub user info: ", userInfo);
-
-    // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
-    console.log("Generating your README next...")
-    const markdown = generateMarkdown(userResponses, userInfo);
-    console.log(markdown);
-
-    // Write markdown to file
-    await writeFileAsync('ExampleREADME.md', markdown);
-
-  } catch (error) {
-    console.log(error);
-  }
-};
-
+// function call to initialize program
 init();
